8902c1b9099b94f4bdbed5828c5bc17b9b6d3889
nmano0006 <nmano0006@gmail.com>
2025-12-29 11:01:07 -0800

    Initial Commit

---
+++ SystemMaintenance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SystemMaintenance
+//
+//  Created by Z790 on 2025-12-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
