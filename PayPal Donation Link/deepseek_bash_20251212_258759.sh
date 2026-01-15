282f07d883906198d90de973bf8b572f891a623a
nmano0006 <58106895+nmano0006@users.noreply.github.com>
2026-01-15 10:47:01 -0800

    Delete PayPal Donation Link directory

--- PayPal Donation Link/deepseek_bash_20251212_258759.sh
+++
@@ -1,9 +0,0 @@
-chmod +x ~/Desktop/kernel-patch-utility-25.2.0.sh
-cd ~/Desktop
-./kernel-patch-utility-25.2.0.sh
-
-
-cp Your-support-enables.sh ~/Desktop/
-chmod +x ~/Desktop/Your-support-enables.sh  
-cd ~/Desktop
-./Your-support-enables.sh
